a566f6afae5796b72a18ef444fb0c3c30313f1fd
Neuroma <jadrenifeni@gmail.com>
2017-10-27 21:13:02 +0100

    6.added cookie,value set to random num(1-20)

--- GuessingGame.go
+++ GuessingGame.go
@@ -5,15 +5,42 @@ package main
 
 import (
 	"html/template"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 type note struct {
 	Message string
 }
 
+//SOURCE: https://golangcode.com/generate-random-numbers/
+func random(min int, max int) int {
+	return rand.Intn(max-min) + min
+}
+
 //SOURCE:http://www.alternatestack.com/development/app-development/simple-http-server-with-templated-response-in-go/
+//https://github.com/data-representation/go-cookies/blob/master/go-cookie.go
 func guessHandler(w http.ResponseWriter, r *http.Request) {
+	count := 0
+	rand.Seed(time.Now().UnixNano())
+	count = random(1, 20)
+
+	// Try to read the cookie.
+	var cookie, err = r.Cookie("target")
+	if err == nil {
+		// If we could read it, try to convert its value to an int.
+		count, _ = strconv.Atoi(cookie.Value)
+	}
+
+	// Create a cookie instance and set the cookie.
+	cookie = &http.Cookie{
+		Name:  "target",
+		Value: strconv.Itoa(count),
+	}
+	http.SetCookie(w, cookie)
+
 	t, _ := template.ParseFiles("guess.html")               // parses guess.html to create a template "t" (?)internally(?)
 	msg := note{Message: "Guess a number between 1 and 20"} // message to pass to templated guess.html
 	t.Execute(w, msg)
@@ -26,7 +53,7 @@ func main() {
 	fs := http.FileServer(http.Dir("./")) //all files are in the same directory as .go file
 	http.Handle("/", fs)
 
-	http.HandleFunc("/guess.html", guessHandler) //invokes this when link "New game" pressed
+	http.HandleFunc("/guess.html", guessHandler) //invoked when "New game" pressed
 
 	http.ListenAndServe(":8080", nil) // port
 
